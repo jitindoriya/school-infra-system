1cc30b13d28e8fee7f7f73715604ec53b355e6d7
kshitij <kshitijhack@gmail.com>
2016-09-23 14:44:26 +0530

    nithing

--- teachers/views.py
+++ teachers/views.py
@@ -21,3 +21,27 @@ def all_teacher_details(request):
     # teacher = get_object_or_404(Teacher)
     all_teacher = Teacher.objects.all()
     return render(request, 'teachers/teachers_details.html', {'all_teacher': all_teacher})
+
+
+class Add_Teacher(View):
+
+    def get(self, request, *args, **kwargs):
+        userform = UserRegistrationForm()
+        teacherform = TeacherForm()
+
+        return render(request,'teachers/addteacher.html',{'userform':userform, 'teacherform':teacherform})
+
+    def post(self, request, *args, **kwargs):
+        userform = UserRegistrationForm(request.POST)
+        teacherform = TeacherForm(request.POST)
+
+        if userform.is_valid and teacherform.is_valid:
+            u = userform.save(commit=False)
+            teacherform.user = u.id
+            teacherform.save()
+            u.save()
+
+            return HttpResponseRedirect('/success/')
+        
+        return render('students/addstudent.html',{'userform':userform, 'studentform':studenform})
+
